951867558731ed44bbaf3093de3b11ab13b74422
Christian Gendreau <cgendreau@gbif.org>
2016-11-04 16:12:38 +0100

    update ValidationResource after review

--- validator-ws/src/main/java/org/gbif/validation/ws/UploadedFileManager.java
+++ validator-ws/src/main/java/org/gbif/validation/ws/UploadedFileManager.java
@@ -8,7 +8,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import com.sun.jersey.multipart.file.DefaultMediaTypePredictor;
@@ -16,6 +18,7 @@ import org.apache.commons.compress.archivers.ArchiveException;
 import org.apache.commons.compress.archivers.ArchiveInputStream;
 import org.apache.commons.compress.archivers.ArchiveStreamFactory;
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,7 +29,11 @@ public class UploadedFileManager {
 
   private static final Logger LOG = LoggerFactory.getLogger(UploadedFileManager.class);
 
-  private final String ZIP_CONTENT_TYPE = DefaultMediaTypePredictor.CommonMediaTypes.ZIP.getMediaType().toString();
+  private static final String ZIP_CONTENT_TYPE = DefaultMediaTypePredictor.CommonMediaTypes.ZIP.getMediaType().toString();
+  private static final String TXT_CONTENT_TYPE = DefaultMediaTypePredictor.CommonMediaTypes.TXT.getMediaType().toString();
+  private static final String CSV_CONTENT_TYPE = "text/csv";
+
+  private final static List<String> TABULAR_CONTENT_TYPE = Arrays.asList(TXT_CONTENT_TYPE, CSV_CONTENT_TYPE);
 
   private final String workingDirectory;
 
@@ -34,7 +41,6 @@ public class UploadedFileManager {
     this.workingDirectory = workingDirectory;
 
     File workingDirectoryFile = new File(workingDirectory);
-
     try {
       if(!workingDirectoryFile.exists()) {
         Files.createDirectory(new File(workingDirectory).toPath());
@@ -55,19 +61,20 @@ public class UploadedFileManager {
    * @return
    * @throws IOException
    */
-  public DataFileDescriptor handleFileUpload(String filename, String contentType, InputStream inputStream) throws IOException {
+  public Optional<DataFileDescriptor> handleFileUpload(String filename, String contentType, InputStream inputStream) throws IOException {
     java.nio.file.Path destinationFolder = generateRandomFolderPath();
     Files.createDirectory(destinationFolder);
 
     DataFileDescriptor dataFileDescriptor = new DataFileDescriptor();
 
-    java.nio.file.Path uploadedResourcePath = null;
+    java.nio.file.Path uploadedResourcePath;
 
     //check if we have something to unzip
     if(contentType.equalsIgnoreCase(ZIP_CONTENT_TYPE)) {
       try {
         unzip(inputStream, destinationFolder);
         uploadedResourcePath = destinationFolder;
+        //a little bit risky to assume that
         dataFileDescriptor.setFormat(FileFormat.DWCA);
       } catch (ArchiveException e) {
         LOG.error("Issue while unzipping data at {}", e, destinationFolder);
@@ -75,16 +82,23 @@ public class UploadedFileManager {
         throw new IOException(e);
       }
     }
-    else{ //copy a single file
-      uploadedResourcePath = destinationFolder.resolve(UUID.randomUUID().toString()+".csv");
-      Files.copy(inputStream, uploadedResourcePath, StandardCopyOption.REPLACE_EXISTING);
+    else if (TABULAR_CONTENT_TYPE.contains(contentType)){
+      //preserve the extension (mostly for debugging)
+      String fileExt = FilenameUtils.getExtension(filename);
+      uploadedResourcePath = destinationFolder.resolve(UUID.randomUUID().toString() + "." + fileExt);
+
+      Files.copy(inputStream, uploadedResourcePath);
       inputStream.close();
       dataFileDescriptor.setFormat(FileFormat.TABULAR);
+    }
+    else {
+      LOG.warn("Unsupported file type: {}", contentType);
+      return Optional.empty();
     }
 
     dataFileDescriptor.setSubmittedFile(filename);
     dataFileDescriptor.setUploadedResourcePath(uploadedResourcePath);
-    return dataFileDescriptor;
+    return Optional.of(dataFileDescriptor);
   }
 
   /**
