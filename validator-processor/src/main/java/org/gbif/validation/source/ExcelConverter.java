ba9f3ecfe5d100de90b778c15ecc59e1ec83ea48
Christian Gendreau <cgendreau@gbif.org>
2016-11-11 14:14:27 +0100

    first version of Excel converter

---
+++ validator-processor/src/main/java/org/gbif/validation/source/ExcelConverter.java
@@ -0,0 +1,174 @@
+package org.gbif.validation.source;
+
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.TimeZone;
+import javax.annotation.Nullable;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.supercsv.io.CsvListWriter;
+import org.supercsv.io.ICsvListWriter;
+import org.supercsv.prefs.CsvPreference;
+
+/**
+ *
+ * This converter reads a Workbook (.xlsx, .xls) file and produces a csv file.
+ *
+ * Inspired from Apache POI example:
+ * http://svn.apache.org/repos/asf/poi/trunk/src/examples/src/org/apache/poi/ss/examples/ToCSV.java
+ */
+public class ExcelConverter {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ExcelConverter.class);
+  private static final TimeZone UTC_TIMEZONE = TimeZone.getTimeZone("UTC");
+
+  private DataFormatter formatter;
+
+  /**
+   * Get a new ExcelConverter instance.
+   *
+   * @return
+   */
+  public static ExcelConverter newConverter(){
+    return new ExcelConverter();
+  }
+
+  private ExcelConverter() {
+    formatter = new DataFormatter(Locale.ENGLISH, true);
+  }
+
+  /**
+   * Convert a workbook (binary .xls or SpreadsheetML .xlsx format)
+   *
+   * @param workbookFile Path to the  workbook file
+   * @param csvFile Path to the csv file to produce
+   * @throws IOException
+   * @throws InvalidFormatException Thrown if invalid xml is found whilst parsing an input SpreadsheetML file.
+   */
+  public void convertToCSV(Path workbookFile, Path csvFile) throws IOException, InvalidFormatException {
+
+    try (FileInputStream fis = new FileInputStream(workbookFile.toFile());
+         ICsvListWriter csvWriter = new CsvListWriter(new FileWriter(csvFile.toFile()),
+                 CsvPreference.STANDARD_PREFERENCE)) {
+
+      Workbook workbook = WorkbookFactory.create(fis);
+      convertToCSV(workbook, csvWriter);
+
+      //ensure to flush remaining content to csvWriter
+      csvWriter.flush();
+    }
+  }
+
+
+  /**
+   *
+   * Convert a {@link Workbook} to a CSV file using {@link ICsvListWriter}
+   *
+   * @param workbook
+   * @param csvWriter
+   * @throws IOException
+   */
+  private void convertToCSV(Workbook workbook, ICsvListWriter csvWriter) throws IOException {
+    Objects.requireNonNull(workbook, "workbook shall be provided");
+    Objects.requireNonNull(csvWriter, "csvWriter shall be provided");
+
+    if( workbook.getNumberOfSheets() == 0 ) {
+      LOG.warn("No sheet found in the workbook");
+      return;
+    } //we work only on one sheet
+    else  if( workbook.getNumberOfSheets() > 1) {
+      LOG.warn("Detected more than 1 sheet, only reading the first one.");
+    }
+
+    Sheet sheet = workbook.getSheetAt(0);
+    FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
+    if(sheet.getPhysicalNumberOfRows() > 0) {
+      Row row;
+      //extract headers (first line)
+      row = sheet.getRow(0);
+      List<String> headers = rowToCSV(row, evaluator, Optional.empty());
+
+      //reverse iteration (from the right side of the Workbook) to remove empty columns
+      ListIterator<String> iterator = headers.listIterator(headers.size());
+      while (iterator.hasPrevious()) {
+        if(StringUtils.isBlank(iterator.previous())){
+          iterator.remove();
+        }
+      }
+      csvWriter.writeHeader(headers.toArray(new String[headers.size()]));
+
+      int lastRowNum = sheet.getLastRowNum();
+      for(int j = 1; j <= lastRowNum; j++) {
+        row = sheet.getRow(j);
+        csvWriter.write(rowToCSV(row, evaluator, Optional.of(headers.size() - 1)));
+        if(j/100 == 0){
+          csvWriter.flush();
+        }
+      }
+    }
+  }
+
+  /**
+   * Called to convert a row of cells into a line of data that can later be
+   * output to the CSV file.
+   *
+   * @param row can be HSSFRow or XSSFRow classes or null
+   * @param evaluator
+   * @param maxColumnIdx
+   * @return
+   */
+  private List<String> rowToCSV(@Nullable Row row, FormulaEvaluator evaluator, Optional<Integer> maxColumnIdx) {
+    List<String> csvLine = new ArrayList<>();
+
+    if(row != null) {
+      Cell cell = null;
+      //we want to loop until maxColumnIdx (if provided) even if it's greater than getLastCellNum()
+      //we shall have the same number of entries on every line in the CSV
+      int maxCellNum = maxColumnIdx.orElse(Short.valueOf(row.getLastCellNum()).intValue());
+      for(int i = 0; i <= maxCellNum; i++) {
+        cell = row.getCell(i);
+        //add an empty string when we have no data
+        if(cell == null) {
+          csvLine.add("");
+        }
+        else {
+          //getCellTypeEnum deprecation explanation: see https://bz.apache.org/bugzilla/show_bug.cgi?id=60228
+          if(cell.getCellTypeEnum() != CellType.FORMULA) {
+            if(cell.getCellTypeEnum() == CellType.NUMERIC && DateUtil.isCellDateFormatted(cell)) {
+              csvLine.add(DateUtil.getJavaDate(cell.getNumericCellValue(), false, UTC_TIMEZONE).toInstant().toString());
+            }
+            else {
+              csvLine.add(this.formatter.formatCellValue(cell));
+            }
+          }
+          //if there is a formula, apply the evluator to get the result
+          else {
+              csvLine.add(this.formatter.formatCellValue(cell, evaluator));
+          }
+        }
+      }
+    }
+    return csvLine;
+  }
+
+}
---
+++ validator-processor/src/test/java/org/gbif/validation/source/ExcelConverterTest.java
@@ -0,0 +1,45 @@
+package org.gbif.validation.source;
+
+import org.gbif.utils.file.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * {@link ExcelConverter} related tests
+ */
+public class ExcelConverterTest {
+
+  @Rule
+  public TemporaryFolder folder = new TemporaryFolder();
+
+  private ExcelConverter EC = ExcelConverter.newConverter();
+
+  private static final String CSV_TEST_FILE_LOCATION = "workbooks/occurrence-workbook.csv";
+  private static final String XLSX_TEST_FILE_LOCATION = "workbooks/occurrence-workbook.xlsx";
+  private static final String XLS_TEST_FILE_LOCATION = "workbooks/occurrence-workbook.xls";
+  //private static final String ODS_TEST_FILE_LOCATION = "workbooks/occurrence-workbook.ods";
+
+  @Test
+  public void testDwcReader() throws IOException, InvalidFormatException {
+    testDwcReader(FileUtils.getClasspathFile(XLSX_TEST_FILE_LOCATION));
+    testDwcReader(FileUtils.getClasspathFile(XLS_TEST_FILE_LOCATION));
+  }
+
+  public void testDwcReader(File workbookFile) throws IOException, InvalidFormatException {
+
+    File testCsvFile = FileUtils.getClasspathFile(CSV_TEST_FILE_LOCATION);
+    File testFile = folder.newFile();
+    EC.convertToCSV(workbookFile.toPath(), testFile.toPath());
+
+    assertTrue(org.apache.commons.io.FileUtils.contentEqualsIgnoreEOL(testFile, testCsvFile, "UTF-8"));
+  }
+
+}
