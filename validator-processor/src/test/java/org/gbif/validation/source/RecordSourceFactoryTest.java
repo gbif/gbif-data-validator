002e6bb8abc2a288cad07ee013834c34ceb36f35
Christian Gendreau <cgendreau@gbif.org>
2016-12-08 09:58:17 +0100

    fix potential issue with Dwc-A Id column ensure it will work if the column is not at index 0

--- validator-processor/src/test/java/org/gbif/validation/source/RecordSourceFactoryTest.java
+++ validator-processor/src/test/java/org/gbif/validation/source/RecordSourceFactoryTest.java
@@ -3,6 +3,7 @@ package org.gbif.validation.source;
 import org.gbif.dwc.terms.DwcTerm;
 import org.gbif.utils.file.FileUtils;
 import org.gbif.validation.api.DataFile;
+import org.gbif.validation.api.RecordSource;
 import org.gbif.validation.api.model.FileFormat;
 
 import java.io.File;
@@ -10,9 +11,11 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit tests for RecordSourceFactory
@@ -37,6 +40,23 @@ public class RecordSourceFactoryTest {
     //all components should points to the parent DataFile
     preparedDataFiles.forEach( df -> assertEquals(dataFile, df.getParent().get()));
 
+    // the filePath should point to the component file
+    assertTrue(StringUtils.endsWith(preparedDataFiles.get(0).getFilePath().toString(), ".txt"));
+  }
+  
+  @Test
+  public void testfromDataFile() throws IOException {
+    DataFile dataFile = new DataFile();
+
+    File testFile = FileUtils.getClasspathFile(TEST_DWC_FILE_LOCATION);
+    dataFile.setFileFormat(FileFormat.DWCA);
+    dataFile.setFilePath(testFile.toPath());
+    List<DataFile> preparedDataFiles = RecordSourceFactory.prepareSource(dataFile);
+
+    DataFile taxonDataFile = preparedDataFiles.stream().filter(df -> df.getRowType() == DwcTerm.Taxon).findFirst().get();
+    try(RecordSource rs = RecordSourceFactory.fromDataFile(taxonDataFile).get()){
+      assertEquals("1559060", rs.read()[0]);
+    }
   }
 
   @Test
