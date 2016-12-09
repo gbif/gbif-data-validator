d1afd7615721144ce734983c562d6b0f18c1605a
Christian Gendreau <cgendreau@gbif.org>
2016-12-09 13:26:41 +0100

    removed unused classes

--- validator-processor/src/main/java/org/gbif/validation/processor/ParallelDataFileProcessorMaster.java
+++ validator-processor/src/main/java/org/gbif/validation/processor/ParallelDataFileProcessorMaster.java
@@ -73,13 +73,6 @@ public class ParallelDataFileProcessorMaster extends AbstractLoggingActor {
     }
   }
 
-  /**
-   * Calculates the number of records that will be processed by each data worker.
-   */
-  private static int recordsPerSplit(int fileSizeInLines, int fileSplitSize){
-    return fileSizeInLines / Math.max(fileSizeInLines/fileSplitSize, 1);
-  }
-
 
   ParallelDataFileProcessorMaster(EvaluatorFactory factory, Integer fileSplitSize, String baseWorkingDir,
                                   ChecklistValidator  checklistValidator) {
