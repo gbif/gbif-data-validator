b872d84d338710fa2e0f7fcb8f064bcb6bbae0a9
Christian Gendreau <cgendreau@gbif.org>
2016-12-13 11:31:12 +0100

    removing unused old classes

--- validator-core/src/main/java/org/gbif/validation/api/DataFileProcessor.java
+++
@@ -1,8 +0,0 @@
-package org.gbif.validation.api;
-
-import org.gbif.validation.api.result.RecordsValidationResultElement;
-
-public interface DataFileProcessor {
-
-  RecordsValidationResultElement process(DataFile dataFile);
-}
