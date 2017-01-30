7696613610c48f0fbbc64b93fc0470d71c324056
Christian Gendreau <cgendreau@gbif.org>
2017-01-30 08:24:54 +0100

    Avoid to use the same file name in tests (FileBashUtilitiesTest)

--- validator-processor/src/test/java/org/gbif/validation/util/FileBashUtilitiesTest.java
+++ validator-processor/src/test/java/org/gbif/validation/util/FileBashUtilitiesTest.java
@@ -23,20 +23,16 @@ public class FileBashUtilitiesTest {
   @Rule
   public TemporaryFolder folder = new TemporaryFolder();
 
-  private File testFile = FileUtils.getClasspathFile("splitter/original_file.csv");
-  private File testFileNoNewline = FileUtils.getClasspathFile("splitter/original_file_no_newline.csv");
+  private File TEST_FILE = FileUtils.getClasspathFile("splitter/original_file.csv");
+  private File TEST_FILE_NONEWLINE = FileUtils.getClasspathFile("splitter/original_file_no_newline.csv");
 
   @Test
   public void testCountLines() throws IOException {
     //won't work on Windows
     assumeTrue(SystemUtils.IS_OS_LINUX || SystemUtils.IS_OS_MAC);
 
-    File t1 = folder.newFile("t1.csv");
-    File t2 = folder.newFile("t2.csv");
-
-    //we copy files since we will change them
-    org.apache.commons.io.FileUtils.copyFile(testFile, t1);
-    org.apache.commons.io.FileUtils.copyFile(testFileNoNewline, t2);
+    File t1 = copyTestFile(TEST_FILE, "count_t1.csv");
+    File t2 = copyTestFile(TEST_FILE_NONEWLINE, "count_t2.csv");
 
     assertEquals(5, FileBashUtilities.countLines(t1.getAbsolutePath()));
     //test the assumption that "wc" will not count the last line of it doesn't end with a newline
@@ -56,12 +52,8 @@ public class FileBashUtilitiesTest {
     //won't work on Windows
     assumeTrue(SystemUtils.IS_OS_LINUX || SystemUtils.IS_OS_MAC);
 
-    File t1 = folder.newFile("t1.csv");
-    File t2 = folder.newFile("t2.csv");
-
-    //we copy files since we will change them
-    org.apache.commons.io.FileUtils.copyFile(testFile, t1);
-    org.apache.commons.io.FileUtils.copyFile(testFileNoNewline, t2);
+    File t1 = copyTestFile(TEST_FILE, "split_t1.csv");
+    File t2 = copyTestFile(TEST_FILE_NONEWLINE, "split_nonewline_t2.csv");
 
     //try first with the file that includes a newline
     assertSplitInNewFolder(t1, 2, 3);
@@ -72,6 +64,22 @@ public class FileBashUtilitiesTest {
     //fix the newline
     FileBashUtilities.ensureEndsWithNewline(t2.getAbsolutePath());
     assertSplitInNewFolder(t2, 2, 3);
+  }
+
+  /**
+   * We need to copy files since we will modify them
+   *
+   * @param testFile
+   * @param fileName
+   *
+   * @return
+   *
+   * @throws IOException
+   */
+  private File copyTestFile(File testFile, String fileName) throws IOException {
+    File testFileCopy = folder.newFile(fileName);
+    org.apache.commons.io.FileUtils.copyFile(testFile, testFileCopy);
+    return testFileCopy;
   }
 
   private void assertSplitInNewFolder(File sourceFile, int splitSize, int expectedNumberOfSplitFile) throws IOException {
