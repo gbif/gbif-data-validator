2be7777d4030c718ef262314618075a8e9816fa4
Christian Gendreau <cgendreau@gbif.org>
2017-07-25 11:33:45 +0200

    changed CSVReader to TabularDataFileReader

--- validator-processor/src/main/java/org/gbif/validation/source/TabularRecordSource.java
+++ validator-processor/src/main/java/org/gbif/validation/source/TabularRecordSource.java
@@ -1,30 +1,31 @@
 package org.gbif.validation.source;
 
 import org.gbif.dwc.terms.Term;
-import org.gbif.utils.file.csv.CSVReader;
+import org.gbif.utils.file.tabular.TabularDataFileReader;
+import org.gbif.utils.file.tabular.TabularFiles;
 import org.gbif.validation.api.RecordSource;
 import org.gbif.validation.api.TabularDataFile;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 import javax.annotation.Nullable;
 
 /**
  * TabularRecordSource allows to expose the content of a {@link TabularDataFile} as {@link RecordSource}.
- * Internally it wraps {@link CSVReader} to ensure compatibility with GBIF crawling.
+ * Internally it wraps {@link TabularDataFileReader} to ensure compatibility with GBIF crawling.
  */
 class TabularRecordSource implements RecordSource {
 
   private final TabularDataFile tabularDataFile;
-  private final CSVReader csvReader;
+  private final TabularDataFileReader<List<String>> tabularReader;
 
   TabularRecordSource(TabularDataFile tabularDataFile) throws IOException {
     this.tabularDataFile = tabularDataFile;
-
-    csvReader = new CSVReader(tabularDataFile.getFilePath().toFile(),
-            tabularDataFile.getCharacterEncoding().name(),
-            tabularDataFile.getDelimiterChar().toString(),
-            tabularDataFile.getQuoteChar(),
-            tabularDataFile.isHasHeaders() ? 1 : 0);
+    tabularReader = TabularFiles.newTabularFileReader(
+            Files.newBufferedReader(tabularDataFile.getFilePath(),
+                    tabularDataFile.getCharacterEncoding()), tabularDataFile.getDelimiterChar(),
+            tabularDataFile.isHasHeaders());
   }
 
   @Nullable
@@ -36,11 +37,17 @@ class TabularRecordSource implements RecordSource {
   @Nullable
   @Override
   public String[] read() throws IOException {
-    return csvReader.next();
+    List<String> line = tabularReader.read();
+
+    if(line == null){
+      return null;
+    }
+    //FIXME we should probably return the List here
+    return line.toArray(new String[line.size()]);
   }
 
   @Override
   public void close() throws IOException {
-    csvReader.close();
+    tabularReader.close();
   }
 }
